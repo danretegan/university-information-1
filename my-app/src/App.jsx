029c13c8ab574c428645d0a0e8b1e5e1f91d325b
Dan Retegan <danretegan@yahoo.com>
2024-01-17 16:15:38 +0200

    cerinte

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,33 @@
+import { Component } from "react";
 import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
+import Tutors from "./components/tutors/Tutors";
+import University from "./components/university/University";
 
-function App() {
-  return <div></div>;
+class App extends Component {
+  tutors = [
+    {
+      id: 1,
+      firstName: "John",
+      lastName: "Smith",
+      telephone: "07123456",
+      email: "johnsmith@company.com",
+      location: "Paris",
+      role: "Administrator",
+    },
+  ];
+
+  render() {
+    return (
+      <main className="App">
+        <Sidebar />
+        <section className="container">
+          <University />
+          <Tutors list={this.tutors} />
+        </section>
+      </main>
+    );
+  }
 }
 
 export default App;
